1415610f04d71304965af4b66dafb8c6c3c83ef9
Lucas Altamirano <lucas.altamirano.v@mail.pucv.cl>
2025-05-10 20:41:02 -0400

    Avance

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import AppRoutes from './routes/AppRoutes';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar></Navbar>
       <AppRoutes />
     </BrowserRouter>
   );
